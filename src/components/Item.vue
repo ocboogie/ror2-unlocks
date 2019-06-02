e5397ae183bc81420e8ad18c156e03330a22a407
ocboogie <boogie@mikulec.com>
2019-06-02 03:40:37 -0700

    Simplify opening items

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,68 +1,16 @@
 <template>
-  <img
-    class="item"
-    width="96"
-    height="96"
-    :src="icon"
-    :alt="name"
-    tabindex="0"
-    @click="openItem"
-    @keyup.enter="openItem"
-  />
+  <img class="item" width="96" height="96" :src="icon" :alt="name" />
 </template>
 <script>
 export default {
   props: {
-    codeName: {
-      type: String,
-      required: true
-    },
     name: {
       type: String,
       required: true
     },
-    tier: {
-      type: String,
-      default: null
-    },
     icon: {
       type: String,
       required: true
-    },
-    description: {
-      type: String,
-      required: true
-    },
-    type: {
-      type: String,
-      required: true
-    },
-    isLunar: {
-      type: Boolean,
-      default: null
-    },
-    unlock: {
-      type: String,
-      default: null
-    },
-    iconName: {
-      type: String,
-      required: true
-    }
-  },
-  methods: {
-    openItem() {
-      this.$emit("openItem", {
-        codeName: this.codeName,
-        name: this.name,
-        tier: this.tier,
-        icon: this.icon,
-        iconName: this.iconName,
-        description: this.description,
-        type: this.type,
-        isLunar: this.isLunar,
-        unlock: this.unlock
-      });
     }
   }
 };
