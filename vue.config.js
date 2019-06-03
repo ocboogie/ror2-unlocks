e46c0fcff562cf502539ada8cbfa61c662d4358d
ocboogie <boogie@mikulec.com>
2019-06-02 19:02:10 -0700

    Make icons not inline with base64

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   lintOnSave: false,
   css: {
@@ -9,5 +11,27 @@ module.exports = {
         data: '@import "~@/variables.scss";'
       }
     }
+  },
+  // Make icons not inline with base64
+  chainWebpack: config => {
+    const iconsPath = path.join(__dirname, "src/assets/icons");
+
+    // Create a new rule just for icons, and use file-loader
+    config.module
+      .rule("icons")
+      .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+      .include.add(iconsPath)
+      .end()
+      .use("file-loader")
+      .loader("file-loader")
+      .options({
+        name: "img/[name].[hash:8].[ext]"
+      });
+
+    // Exclude icons from the origin rule
+    config.module
+      .rule("images")
+      .exclude.add(iconsPath)
+      .end();
   }
 };
