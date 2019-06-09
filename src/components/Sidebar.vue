d54db562e1ebb9873a70e3013dcc7bc640f6c72f
ocboogie <boogie@mikulec.com>
2019-06-08 18:24:53 -0700

    Use g-image for the game banner

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -50,7 +50,7 @@
         access version in March 2019, with Hopoo Games anticipating about a year
         of work before they push a final release.
       </div>
-      <img class="game-banner" src="../assets/ROR2Banner.jpg" />
+      <g-image class="game-banner" src="../assets/ROR2Banner.jpg" />
     </template>
   </div>
 </template>
